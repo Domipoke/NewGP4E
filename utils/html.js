94b6f5a8b1f28b9e64a76f499048bb8a8e7d20f1
Domipoke <domenico.montuori1312006@gmail.com>
2022-04-22 14:02:50 +0200

    update

--- utils/html.js
+++ utils/html.js
@@ -1,9 +1,97 @@
 var pos = [
+    "bgcolor",
+    "bgcred","bgcgreen","bgcpurple","bgcwhite",
     "page",
     "slide",
     "menu"
 ]
+class BgColor extends HTMLElement {
+    constructor(props) {
+        super(props);
+        this.style.zIndex=pos.indexOf("bgcolor")
+        this.style.position="relative"
+        this.style.display="inline"
+        this.style.overflow="none"
+        this.style.width="100%"
+        this.style.height="100%"
 
+        //
+        var tl=document.createElement("div") 
+        var tr=document.createElement("div")
+        var br=document.createElement("div") 
+        var bl=document.createElement("div")
+        
+        tl.style.backgroundImage="linear-gradient(135deg, rgba(219,52,52,1) 0%, rgba(255,255,255,1) 100%)"
+        tr.style.backgroundImage="linear-gradient(225deg, rgba(52,219,143,1) 0%, rgba(255,255,255,1) 100%)"
+        br.style.backgroundImage="linear-gradient(315deg, rgba(212,212,212,1) 0%, rgba(255,255,255,1) 100%)"
+        bl.style.backgroundImage="linear-gradient(315deg, rgba(212,212,212,1) 0%, rgba(255,255,255,1) 100%)"
+        
+        tl.style.display="block"
+        tr.style.display="block"
+        bl.style.display="block"
+        br.style.display="block"
+
+        tl.style.opacity="0.5"
+        tr.style.opacity="0.5"
+        bl.style.opacity="0.5"
+        br.style.opacity="0.5"
+
+        //tl.style.margin="0"
+        //tr.style.margin="0"
+        //bl.style.margin="0"
+        //br.style.margin="0"
+
+        //tl.style.padding="0"
+        //tr.style.padding="0"
+        //bl.style.padding="0"
+        //br.style.padding="0"
+
+        tl.style.position="absolute"
+        tr.style.position="absolute"
+        bl.style.position="absolute"
+        br.style.position="absolute"
+
+        tl.style.width="100%"
+        tr.style.width="100%"
+        bl.style.width="100%"
+        br.style.width="100%"
+
+        tl.style.height="100%"
+        tr.style.height="100%"
+        bl.style.height="100%"
+        br.style.height="100%"
+
+        tl.style.left="0"
+        tr.style.left="0"
+        bl.style.left="0"
+        br.style.left="0"
+
+        tl.style.top="0"
+        tr.style.top="0"
+        bl.style.top="0"
+        br.style.top="0"
+
+        tl.style.overflow="none"
+        tr.style.overflow="none"
+        bl.style.overflow="none"
+        br.style.overflow="none"
+        
+        //tl.style.float="left"
+        //tr.style.float="right"
+        //bl.style.float="left"
+        //br.style.float="right"
+
+        tl.style.zIndex=pos.indexOf("bgcred")
+        tr.style.zIndex=pos.indexOf("bgcgreen")
+        bl.style.zIndex=pos.indexOf("bgcpurple")
+        br.style.zIndex=pos.indexOf("bgcwhite")
+
+        this.appendChild(tl)
+        this.appendChild(tr)
+        this.appendChild(br)
+        this.appendChild(bl)
+    }
+}
 class Gp4eTitle extends HTMLElement {
     constructor(props){
         super(props)
@@ -18,7 +106,6 @@ class MenuContainer extends HTMLElement {
         this.style.top=0
         this.style.left=0
         this.style.width="100vw"
-        this.style.height="100%"
         this.style.zIndex=pos.indexOf("menu")
         this.style.backgroundColor="none"
         this.style.display="block"
@@ -47,15 +134,16 @@ class MenuItem extends HTMLElement {
         this.style.cursor="default"
         //Set Text
         switch(this.getAttribute("type")){
-            case "page":
-                this.setText(this.getAttribute("name"))
+            case "link":
+                console.log(this.getAttribute("to"))
+                this.setText(this.getAttribute("to"))
+                break;
             case "title":
                 this.setText("<gp4e-title/>")
+                break;
         }
     }
-    setText(text) {
-        this.innerHTML=text
-    }
+    setText(t){this.innerHTML=t}
 }
 
 
@@ -70,7 +158,7 @@ class SitePage extends HTMLElement {
         this.style.zIndex=pos.indexOf("page")
         this.style.margin=0
         this.style.padding=0
-        
+        this.style.userSelect="none"
     }
 }
 class Slide extends HTMLElement {
@@ -88,3 +176,4 @@ customElements.define("gp4e-menu-container",MenuContainer)
 customElements.define("gp4e-menu-item",MenuItem)
 customElements.define("gp4e-page",SitePage)
 customElements.define("gp4e-slide",Slide)
+customElements.define("gp4e-bg",BgColor)
